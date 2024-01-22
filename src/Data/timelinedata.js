6f6b1c48ab69ea7c55e462234444b44c582d5484
gladwin2802 <ajgladwin2802@gmail.com>
2024-01-22 14:29:01 +0530

    completed committee and webdevs section

---
+++ src/Data/timelinedata.js
@@ -0,0 +1,138 @@
+export const events = [
+ 
+  {
+    title: "CSS battle",
+    time: "9:00 AM",
+    date: "March 10",
+  },
+  {
+    title: "Treasure hunt",
+    time: "10:00 AM",
+    date: "March 10",
+  },
+  {
+    title: "Jumbled Code",
+    time: "10:00 AM",
+    date: "March 10",
+  },
+  {
+    title: "UI design contest",
+    time: "11:00 AM",
+    date: "March 10",
+  },
+  {
+    title: "Attack on Anime",
+    time: "12:15 PM",
+    date: "March 10",
+  },
+  {
+    title: "Sales Talk",
+    time: "12:15 PM",
+    date: "March 10",
+  },
+  {
+    title: "Reverse Coding",
+    time: "12:15 PM",
+    date: "March 10",
+  },
+  {
+    title: "PC building",
+    time: "2:00 PM",
+    date: "March 10",
+  },
+  {
+    title: "Tech Feud",
+    time: "2:30 PM",
+    date: "March 10",
+  },
+  {
+    title: "Blind Code",
+    time: "3:00 PM",
+    date: "March 10",
+  },
+  {
+    title: "Chess ",
+    time: "6:00 PM",
+    date: "March 10",
+  },
+  {
+    title: "Capture The Flag",
+    time: "6:00 PM",
+    date: "March 10",
+  },
+  {
+    title: "Free fire ",
+    time: "6:00 PM",
+    date: "March 10",
+  },
+  {
+    title: "Valorant ",
+    time: "7:00 PM",
+    date: "March 10",
+  },
+  {
+    title: "Type Racer",
+    time: "9:00 AM",
+    date: "March 11",
+  },
+  {
+    title: "Prompt Engineering",
+    time: "9:30 AM",
+    date: "March 11",
+  },
+  {
+    title: "Competitive Coding",
+    time: "10:30 AM",
+    date: "March 11",
+  },
+  {
+    title: "Poster Design",
+    time: "11:00 AM",
+    date: "March 11",
+  },
+  {
+    title: "Marvel/DC",
+    time: "11:00 AM",
+    date: "March 11",
+  },
+  {
+    title: "Sales Talk",
+    time: "12:15 PM",
+    date: "March 11",
+  },
+  {
+    title: "Hackathon",
+    time: "2:00 PM",
+    date: "March 11",
+  },
+  {
+    title: "Missing Code",
+    time: "2:00 PM",
+    date: "March 11",
+  },
+  {
+    title: "Tech Feud",
+    time: "2:30 PM",
+    date: "March 11",
+  },
+  {
+    title: "Debugging",
+    time: "3:00 PM",
+    date: "March 11",
+  },
+  {
+    title: "Movie Quiz",
+    time: "4:00 PM",
+    date: "March 11",
+  },
+  {
+    title: "Free fire ",
+    time: "6:00 PM",
+    date: "March 11",
+  },
+  {
+    title: "Valorant",
+    time: "6:00 PM",
+    date: "March 11",
+  },
+];
