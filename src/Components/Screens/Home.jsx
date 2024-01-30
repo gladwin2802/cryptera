1ea8a245e2e3690b2b3f3a9a83ec7d5d1526605f
Aniruth19 <m.aniruthan@gmail.com>
2024-01-30 10:08:50 +0530

    Carousel added - changes in Home and events page

--- src/Components/Screens/Home.jsx
+++ src/Components/Screens/Home.jsx
@@ -1,84 +1,68 @@
-import React, { useEffect, useRef } from 'react'
-import '../../Styles/Home.css'
-import * as LottiePlayer from '@lottiefiles/lottie-player'
-import { Navigate, useNavigate } from 'react-router-dom'
-import Button from '../../Components/Button'
-import r from "../Rocket.json";
+import React, { useRef } from 'react';
+import '../../Styles/Home.css';
+import { useNavigate } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel'; 
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
+
 function Home() {
-    const navigator = useNavigate()
-    const [flag, setFlag] = React.useState(false)
-    const Homeref = useRef(null)
-    const greenref = useRef(null)
-    let h = 10
-    const clickhandler = (e) => {
-        navigator("/events")
-    }
-    const cursorhandler = (event) => {
-        const screenwidth = window.innerWidth
-        if (
-            event.clientX > screenwidth / 2
-        ) {
-            // alert("Hello")
-        }
+  const navigator = useNavigate();
+  const Homeref = useRef(null);
+  const greenref = useRef(null);
+  const isMobile = window.innerWidth <= 768; 
 
-    }
-    const wheelhandler = (event) => {
-       // console.log('wheel')
-      //  console.log(event.deltaY)
-        if (event.deltaY < 0) {
-            h = 0
-            greenref.current.style.height = "0px"
-        }
-        else {
-            if (Homeref.current.offsetHeight + Homeref.current.scrollTop >= Homeref.current.scrollHeight) {
-                h += 3
-               // console.log(h)
-                greenref.current.style.height = h + 'px'
-                if (h > 60) {
-                    greenref.current.style.height = '0px'
-                    navigator('/about_us')
-                }
-            }
-        }
+  const clickhandler = (category) => {
+    
+    navigator(`/events?category=${encodeURIComponent(category)}`);
+  };
 
+  function getImagePath(index) {
+    switch (index) {
+      case 0:
+        return isMobile ? 'pback.jpg' : 'back.jpg';
+      case 1:
+        return isMobile ? 'pback.jpg' : 'back.jpg';
+      case 2:
+        return isMobile ? 'pback.jpg' : 'back.jpg';
+      default:
+        return '';
     }
+  }
 
-    return (
-        <div className='home main-section' id="Home" ref={Homeref} onWheel={wheelhandler}
-            onMouseOver={cursorhandler} style={{ position: "relative" }}
+  return (
+    <div className='home main-section' id='Home' style={{ position: 'relative' }}>
+      <div style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+        <Carousel
+          style={{ height: '100vh' }}
+          dynamicHeight={true}
+          autoPlay={true}
+          interval={2000}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
         >
-            <div className='mobile-container'>
-                <a href='tel:+91 9962623236'>
-                    <i className='fa fa-phone'></i>
-                </a>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <div key={index} style={{ position: 'relative', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <img
+                src={getImagePath(index)}
+                style={{ width: '100%', height: '100%', objectFit: 'cover', maxHeight: "900px" }}
+                alt={`Image ${index + 1}`}
+              />
+              <div className="legend" style={{ maxWidth: "300px", position: 'absolute', textAlign: 'center', marginLeft: "auto", marginRight: "auto", left: 0, right: 0 ,background: 'linear-gradient(135deg, rgba(144, 238, 144, 0.5), rgba(128, 0, 128, 0.5))',
+                opacity: 0.9, marginBottom: "70px", fontSize: "20px" }}>
+                {index === 0 ? 
+                  <button onClick={() => clickhandler('Technical')}>Technical</button> :
+                  index === 1 ? 
+                  <button onClick={() => clickhandler('Non-Technical')}>Non-Technical</button> :
+                  <button onClick={() => clickhandler('Flagship')}>Flagship</button>
+                }
+              </div>
             </div>
-            <div className='home-content'>
-                <div className='home-content-text'>
-                    <h1>A National level Technical Symposium conducted by CSE department of CIT.</h1>
-                    {/* <div className='explore-btn'></div> */}
-                    <div className='explore-btn'>
-                        <Button text="Check out the Events" onClick={clickhandler} />
-                    </div>
-
-                </div>
-                <div className='lottie'>
-                    <lottie-player
-                        autoplay
-                        loop
-                        mode="normal"
-                        src="https://assets3.lottiefiles.com/packages/lf20_yogqMMhCr0.json"
-                    ></lottie-player>
-                </div>
-
-            </div>
-            {/* <div className='add-green bg-gradient-to-tr from-green-600 to-green-300 ' ref={greenref} /> */}
-
-
-
-
-
-        </div>
-    )
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
+
